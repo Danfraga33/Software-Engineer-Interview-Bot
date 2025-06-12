a1c6eb548767cdf132bfd45901bc4a4ead62d6e4
Daniel Fraga <dan.fraga33@gmail.com>
2025-06-12 22:23:27 +1000

    chore: change vite configfor fly.dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
-import path from 'path';
-// @ts-ignore
-// import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// No remix plugin ?
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
+	server: {
+		host: '0.0.0.0',
+		port: process.env.PORT ? parseInt(process.env.PORT) : 5175,
+	},
+	preview: {
+		host: '0.0.0.0',
+		port: process.env.PORT ? parseInt(process.env.PORT) : 5175,
+		allowedHosts: ['software-engineer-interview-bot.fly.dev'],
 	},
 });
